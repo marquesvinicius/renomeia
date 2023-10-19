f06fdc696a75a4a1147736d2a769ec073cd2f713
Marques <marquesviniciusmelomartins@gmail.com>
2023-10-19 17:58:44 -0300

    html basico, css basico, implementacao das funcoes

---
+++ main.js
@@ -0,0 +1,78 @@
+function processFile() {
+  const fileInput = document.getElementById('fileInput');
+
+  if (fileInput.files.length === 0) {
+    alert('Por favor, selecione um arquivo CSV.');
+    return;
+  }
+
+  const file = fileInput.files[0];
+
+  const reader = new FileReader();
+  reader.onload = function (event) {
+    const contents = event.target.result;
+
+    console.log('Conteúdo do arquivo CSV:', contents); // Adicione esta linha
+
+    Papa.parse(contents, {
+      header: true, // Indica que a primeira linha contém títulos das colunas
+      skipEmptyLines: 'greedy', // Pula linhas em branco
+      dynamicTyping: true, // Tenta converter os valores para o tipo apropriado
+
+      complete: function (results) {
+        const data = results.data;
+
+        if (data.length <= 0) {
+          alert('A tabela não contém dados.');
+          return;
+        }
+
+        const renamedImages = [];
+        const numeroDeImagens = data.length;
+
+        console.log('Número de imagens a processar:', numeroDeImagens); // Adicione esta linha
+
+        for (const row of data) {
+          // Verifique se a linha tem pelo menos duas colunas com dados
+          if (row[0] && row[1]) {
+            const nome = row[0];
+            const url = row[1];
+            renamedImages.push({ nome, url });
+          }
+        }
+
+        console.log('Dados das imagens renomeadas:', renamedImages); // Adicione esta linha
+
+        const zip = new JSZip();
+        const imagePromises = [];
+
+        renamedImages.forEach((image) => {
+          const imagePromise = fetch(image.url)
+            .then((response) => response.blob())
+            .then((blob) => {
+              zip.file(`${image.nome}.jpg`, blob);
+            });
+
+          imagePromises.push(imagePromise);
+        });
+
+        // Aguarda o download de todas as imagens
+        Promise.all(imagePromises).then(() => {
+          console.log('Todas as imagens foram baixadas com sucesso.');
+
+          // Gera o arquivo ZIP e cria o link de download
+          zip.generateAsync({ type: 'blob' }).then((content) => {
+            const url = URL.createObjectURL(content);
+            const downloadLink = document.getElementById('downloadLink');
+            downloadLink.href = url;
+            downloadLink.download = 'imagens_renomeadas.zip';
+            downloadLink.textContent = `Clique aqui para baixar as ${numeroDeImagens} imagens renomeadas`;
+            downloadLink.style.display = 'block';
+          });
+        });
+      },
+    });
+  };
+
+  reader.readAsText(file);
+}
